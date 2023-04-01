bc62dbb150255a30f9af2ed41d85b82b00ec04e9
Your Name <you@example.com>
2023-04-02 01:05:20 +0500

    searching

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\SearchController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/people', [SearchController::class, 'index']);
+Route::get('/people/simple', [SearchController::class, 'simple'])->name('simple_search');
+Route::get('/people/advance', [SearchController::class, 'advance'])->name('advance_search');
